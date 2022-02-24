892219911bf835d0b75f48cefd9a2c2c27f798a4
DeepShade <pheath13797@gmail.com>
2022-02-24 13:04:44 -0700

    Third Commit
    Began adding StickMan to screen

--- src/Blank.java
+++ src/Blank.java
@@ -1,9 +1,12 @@
+import acm.graphics.GLabel;
 import acm.graphics.GRect;
 
 public class Blank extends GRect {
     public static final int WIDTH = 50;
     public static final int HEIGHT = 5;
-    public Blank(double x, double y, char targetLetter) {
+    public Blank(double x, double y) {
         super(x, y, WIDTH,HEIGHT);
     }
+
+
 }
--- src/HangMan.java
+++ src/HangMan.java
@@ -1,32 +1,46 @@
-import acm.graphics.GRect;
+import acm.graphics.GLabel;
+import acm.graphics.GLine;
+import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
 import svu.csc213.Dialog;
 
-import javax.swing.*;
-import java.awt.event.ActionEvent;
 import java.util.Locale;
 import java.util.Random;
+import acm.graphics.GObject;
 
 public class HangMan extends GraphicsProgram {
 
     private String[] wordsArr = {"bunny", "bus", "sublime"};
     private Random rand;
     public String targetWord;
-    private Blank blank;
+    public Blank blank;
+    private Blank[] blanks;
+    private int correctCount, limbsGone;
+
+    private GOval head;
+    private GLine torso, lArm, rArm, lLeg, rLeg;
 
     @Override
     public void init() {
+        addStickMan()
+        correctCount = 0;
+        limbsGone = 0;
         rand = new Random();
         targetWord = wordsArr[rand.nextInt(3)];
+        blanks = new Blank[targetWord.length()];
 
         for (int i = 0; i < targetWord.length(); i++) {
 
-            double blankX = (getWidth()/2 - (blank.WIDTH)+15)*(targetWord.length()/2) + (blank.WIDTH+15)*i;
-            blank = new Blank(blankX, 300, targetWord.charAt(i));
-            blank.setFilled(true);
-            add(blank);
+            double blankX = (getWidth()/2) - (blank.WIDTH+15)*(targetWord.length()/2) + (blank.WIDTH + 15)*i;
+            blanks[i] = new Blank(blankX, 300);
+            blanks[i].setFilled(true);
+            add(blanks[i]);
         }
 
+
+    }
+
+    private void addStickMan() {
 
     }
 
@@ -121,22 +135,57 @@ public class HangMan extends GraphicsProgram {
     public void run() {
         System.out.println(targetWord);
 
-        getGuess();
-            for (int i = 0; i < targetWord.length(); i++) {
-
+        while(limbsGone != 10) {
+            if(!checkAnswer()) {
+                limbsGone++;
+                handleLimbLoss;
+            }
+            if(correctCount == targetWord.length()) {
+                Dialog.showMessage("You did it!");
+                stop();
             }
 
+        }
+        Dialog.showMessage("Oops! You didn't guess it!");
+        stop();
     }
 
-    private char getGuess() {
+    public boolean checkAnswer() {
+        boolean correct = false;
+        String guess = getGuess();
+
+        for (int i = 0; i < targetWord.length(); i++) {
+            if(targetWord.charAt(i) == guess.charAt(0)) {
+                GLabel letterLabel = new GLabel(guess);
+                double letterLabelX = blanks[i].getX() + blanks[i].getWidth()/2 - letterLabel.getWidth()/2;
+                letterLabel.setLocation(letterLabelX,blanks[0].getY() - 10);
+                if(this.getElementAt(letterLabel.getX(), letterLabel.getY()) != null) {
+                    Dialog.showMessage("You already guessed that.");
+                    checkAnswer();
+                    correct = true;
+                    break;
+                }
+                add(letterLabel);
+                correct  = true;
+                correctCount++;
+            }
+        }
+
+        return correct;
+    }
+
+    private String getGuess() {
         String letter = Dialog.getString("What letter do you guess?");
 
-        if(letter.length() > 1) {
+        if(letter.length() > 1 || letter.length() < 1) {
             Dialog.showMessage("Sorry, please enter a single letter");
             getGuess();
-        } else {
-            return letter;
         }
+        return letter;
+    }
+
+    private void handleLimbLoss() {
+
     }
 
     public static void main(String[] args) {
